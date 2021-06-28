90b39c9673a20c3ec40a7e1bc72ae6b965faaf02
fabiopiro <piroddifabio01@gmail.com>
2021-06-28 14:11:02 +0200

    first push

---
+++ snack3/index.php
@@ -0,0 +1,24 @@
+<!-- PHP -->
+<?php
+/*
+# Snack 3
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+*/
+
+?>
+<!-- /PHP -->
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snack3</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
+<!-- /HTML -->
